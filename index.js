08a636187f030eb100a0048003e6f070abe1f547
kwshal <kushal3738@gmail.com>
2025-01-08 00:33:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Get essential HTML elements and store them in relevant variables
+const board = document.getElementById('board');
+const dice = document.getElementById('dice');
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+const message = document.getElementById('message');
